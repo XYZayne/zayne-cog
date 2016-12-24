ec98b336ef9acd9f2b3be89d29d32e2ca4720958
XYZayne <badluckkitty@reborn.com>
2016-12-23 17:16:34 -0800

    Update boop.py

--- boop/boop.py
+++ boop/boop.py
@@ -1,18 +1,19 @@
 import discord
 from discord.ext import commands
 
-class boop:
-    """For all your booping needs!"""
+class Mycog:
+    """My custom cog that does stuff!"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def boop(self, user : discord.Member):
-        """Boop a user!"""
+        """This does stuff!"""
 
         #Your code will go here
-        await self.bot.say("*boops*" + user.mention +)
+        await self.bot.say("*boops" + user.mention + "*")
+
 
 def setup(bot):
-    bot.add_cog(boop(bot))
+    bot.add_cog(Mycog(bot))
